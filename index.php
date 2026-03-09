713b69ce2bd503629d2f751d2be66a76020dda9f
nemethkoppany <koppany344@gmail.com>
2026-03-09 12:52:25 +0100

    orai dolgok

--- index.php
+++ index.php
@@ -55,6 +55,52 @@ if ($jegy == 5) {
     echo "Hibás jegy!";
 }
 
+//Össezgzés
+$szamok = [5, 8, 3, 10, 2];
 
+$osszeg = 0;
 
+foreach ($szamok as $szam) {
+    $osszeg += $szam;
+}
+
+echo " <br>Az összeg: " . $osszeg . "<br>";
+
+//Minimum-maximum
+$szamok = [5, 8, 3, 10, 2];
+
+$min = $szamok[0];
+$max = $szamok[0];
+
+foreach ($szamok as $szam) {
+    if ($szam < $min) {
+        $min = $szam;
+    }
+
+    if ($szam > $max) {
+        $max = $szam;
+    }
+}
+
+echo "Minimum: $min <br>";
+echo "Maximum: $max <br>";
+
+//Lineáris keresés
+$szamok = [5, 8, 3, 10, 2];
+$keresett = 3;
+
+$talalt = false;
+
+foreach ($szamok as $szam) {
+    if ($szam == $keresett) {
+        $talalt = true;
+        break;
+    }
+}
+
+if ($talalt) {
+    echo "A szám megtalálható.";
+} else {
+    echo "A szám nincs a tömbben.";
+}
 ?>
