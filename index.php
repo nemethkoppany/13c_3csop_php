082cc12a8559063ac678947235eb73369dfd09a9
nemethkoppany <koppany344@gmail.com>
2026-03-12 08:47:02 +0100

    extra órao dolgok

--- index.php
+++ index.php
@@ -48,4 +48,95 @@ if ($talalt) {
 } else {
     echo "A szám nincs a tömbben.";
 }
+
+// Összegzés tétele
+function osszegzes($tomb) {
+    $osszeg = 0;
+    foreach ($tomb as $ertek) {
+        $osszeg += $ertek;
+    }
+    return $osszeg;
+}
+ 
+// Maximális kiválasztás tétele
+function maximalisKivalasztas($tomb) {
+    $max = $tomb[0];
+    $index = 0;
+ 
+    foreach ($tomb as $i => $ertek) {
+        if ($ertek > $max) {
+            $max = $ertek;
+            $index = $i;
+        }
+    }
+ 
+    return array($max, $index);
+}
+ 
+// Buborék rendezés
+function buborekRendezes($tomb) {
+    $n = count($tomb);
+ 
+    for ($i = 0; $i < $n - 1; $i++) {
+        for ($j = 0; $j < $n - $i - 1; $j++) {
+            if ($tomb[$j] > $tomb[$j + 1]) {
+                $temp = $tomb[$j];
+                $tomb[$j] = $tomb[$j + 1];
+                $tomb[$j + 1] = $temp;
+            }
+        }
+    }
+ 
+    return $tomb;
+}
+ 
+// Bináris keresés (rendezett tömb kell!)
+function binarisKereses($tomb, $keresett) {
+    $bal = 0;
+    $jobb = count($tomb) - 1;
+ 
+    while ($bal <= $jobb) {
+        $kozep = floor(($bal + $jobb) / 2);
+ 
+        if ($tomb[$kozep] == $keresett) {
+            return $kozep;
+        }
+ 
+        if ($tomb[$kozep] < $keresett) {
+            $bal = $kozep + 1;
+        } else {
+            $jobb = $kozep - 1;
+        }
+    }
+ 
+    return -1;
+}
+ 
+// Logaritmikus keresés (itt gyakorlatilag bináris keresés)
+function logaritmikusKereses($tomb, $keresett) {
+    return binarisKereses($tomb, $keresett);
+}
+ 
+ 
+// Példa használat
+$tomb = array(5, 3, 8, 1, 4);
+ 
+echo "Összeg: " . osszegzes($tomb) . "<br>";
+ 
+list($max, $index) = maximalisKivalasztas($tomb);
+echo "Max érték: $max, index: $index <br>";
+ 
+$rendezett = buborekRendezes($tomb);
+echo "Buborék rendezés: ";
+print_r($rendezett);
+echo "<br>";
+ 
+$keresett = 4;
+$index = binarisKereses($rendezett, $keresett);
+ 
+if ($index != -1) {
+    echo "Bináris keresés - index: $index <br>";
+} else {
+    echo "Nem található <br>";
+}
 ?>
